dac56e5e11a9b3b45bfaeafb8baf80dd25eefd80
Dmitrii <80770507+freezyy1@users.noreply.github.com>
2022-03-31 21:59:34 +0500

    creating database and clearing garbage

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # импортируем необходимые библиотеки
-import requests
 import telebot
 from telebot import types
 # импортируем данные для работы бота
-from tokens import url, api_weather, api_telegram, url_school, city, admin
+from tokens import api_telegram, url_school
 from checker import checker
 from parser_for_site import news_list
-photo = open('imgs/rasp_zvon.JPG', 'rb')
+from database import delete, upload
+from weather import weather
 bot = telebot.TeleBot(api_telegram)
 
 
@@ -17,6 +17,18 @@ def welcome(message):
                      'нажмите /help чтобы узнать команды, которые поддерживает бот')
 
 
+@bot.message_handler(commands=['unsubscribe'])
+def unsubscribe(message):
+    # смотрите в database.py
+    delete(message)
+
+
+@bot.message_handler(commands=['subscribe'])
+def subscribe(message):
+    # смотрите в database.py
+    upload(message)
+
+
 @bot.message_handler(commands=['help'])
 def welcome(message):
     bot.send_message(message.chat.id,
@@ -25,68 +37,21 @@ def welcome(message):
                      '\n/timetable_middle - узнать расписание в средней школе'
                      '\n/timetable_high - узнать расписание в средней школе'
                      '\n/web - сайт школы'
-                     '\n/zvonki - расписание звонков')
+                     '\n/zvonki - расписание звонков'
+                     '\n/subscribe - подписаться на рассылку новостей'
+                     '\n/unsubscribe - отписаться от рассылки новостей'
+                     )
 
 
 @bot.message_handler(commands=['weather'])
-def test(message):
-    try:
-        # создаем глобальные переменные чтобы не было проблем с выводом локальных переменных в будущем
-        global wind
-        global temp
-        # здесь можно было бы ввести вместо Тюмень message.text, чтобы отслеживать сообщения и по ним передавать
-        # данные о погоде
-        city_name = city
-
-        params = {'APPID': api_weather, 'q': city_name, 'units': 'metric', 'lang': 'ru'}
-        result = requests.get(url, params=params)
-        weather = result.json()
-
-        # условие для температуры воздуха для выявление актировки
-        if weather["main"]['temp'] > 29:
-            temp = "Сейчас прохладно"
-        elif weather["main"]['temp'] < 29:
-            temp = "Сегодня холодно"
-        elif weather["main"]['temp'] < 32:
-            temp = "сегодня актировка у 1-9 классов"
-        elif weather["main"]['temp'] < 36:
-            temp = "Также у 10 и 11 классов"
-
-        # условие для скорости ветра
-        if weather["wind"]['speed'] == 0:
-            wind = "По шкале Бофорта сейчас Штиль"
-        elif 1.5 >= weather["wind"]['speed'] >= 0.5:
-            wind = "По шкале Бофорта сейчас тихий ветер "
-        elif 1.6 <= weather["wind"]['speed'] <= 3.3:
-            wind = "По шкале Бофорта сейчас легкий ветер"
-        elif 3.4 <= weather["wind"]['speed'] <= 5.4:
-            wind = "По шкале Бофорта сейчас слабый ветер"
-        elif 5.5 <= weather["wind"]['speed'] <= 7.9:
-            wind = "По шкале Бофорта сейчас умеренный ветер"
-        elif 8.0 <= weather["wind"]['speed'] <= 10.7:
-            wind = "По шкале Бофорта сейчас свежий ветер"
-        elif 10.8 <= weather["wind"]['speed'] <= 13.8:
-            wind = "По шкале Бофорта сейчас сильный ветер"
-
-        bot.send_message(message.chat.id,
-                         "В городе " + str(weather["name"]) + " температура: " + str(
-                             int(weather["main"]['temp'])) + '°' + "\n" +
-                         "Максимальная температура:" + str(int(weather['main']['temp_max'])) + "\n" +
-                         "Минимальная температура: " + str(int(weather['main']['temp_min'])) + "\n" +
-                         "Скорость ветра: " + str(int(weather['wind']['speed'])) + ' м/c' + "\n" +
-                         "Давление: " + str(int(weather['main']['pressure'])) + 'мм рт. ст.' + "\n" +
-                         "Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" +
-                         "Видимость: " + str(weather['visibility']) + "\n" +
-                         "Описание: " + str(weather['weather'][0]["description"]) + "\n" + temp + "\n" + wind + '\n')
-    except:
-        #bot.send_message(message.chat.id, 'вы ввели город, которого нет в базе данных \n')
-        # bot.send_message(message.chat.id, 'рестарт... \n')
-        #bot.send_message(message.chat.id, 'введите название города')
-        bot.send_message(message.chat.id, f'похоже что-то не так с api погоды\n напишите пожалуйста нашему админу{admin}')
+def weather_main(message):
+    # смотрите weather.py
+    weather(message)
 
 
 @bot.message_handler(commands=['zvonki'])
 def zvon(message):
+    photo = open('imgs/rasp_zvon.JPG', 'rb')
     bot.send_photo(message.chat.id, photo)
 
 
@@ -129,6 +94,7 @@ def process_start(message):
 
 @bot.message_handler(content_types=['text'])
 def step1(message):
+    # смотрите в checker.py
     checker(message)
 
 
---
+++ weather.py
@@ -0,0 +1,60 @@
+import requests
+import telebot
+from tokens import url, api_weather, api_telegram, city, admin
+bot = telebot.TeleBot(api_telegram)
+from colorama import Fore
+
+
+def weather(message):
+    try:
+        # здесь можно было бы ввести вместо Тюмень message.text, чтобы отслеживать сообщения и по ним передавать
+        # данные о погоде
+        city_name = city
+
+        params = {'APPID': api_weather, 'q': city_name, 'units': 'metric', 'lang': 'ru'}
+        result = requests.get(url, params=params)
+        weather = result.json()
+
+        # условие для температуры воздуха для выявление актировки
+        if weather["main"]['temp'] > 29:
+            temp = "Сейчас прохладно"
+        elif weather["main"]['temp'] < 29:
+            temp = "Сегодня холодно"
+        elif weather["main"]['temp'] < 32:
+            temp = "сегодня актировка у 1-9 классов"
+        elif weather["main"]['temp'] < 36:
+            temp = "Также у 10 и 11 классов"
+
+        # условие для скорости ветра
+        if weather["wind"]['speed'] == 0:
+            wind = "По шкале Бофорта сейчас Штиль"
+        elif 1.5 >= weather["wind"]['speed'] >= 0.5:
+            wind = "По шкале Бофорта сейчас тихий ветер "
+        elif 1.6 <= weather["wind"]['speed'] <= 3.3:
+            wind = "По шкале Бофорта сейчас легкий ветер"
+        elif 3.4 <= weather["wind"]['speed'] <= 5.4:
+            wind = "По шкале Бофорта сейчас слабый ветер"
+        elif 5.5 <= weather["wind"]['speed'] <= 7.9:
+            wind = "По шкале Бофорта сейчас умеренный ветер"
+        elif 8.0 <= weather["wind"]['speed'] <= 10.7:
+            wind = "По шкале Бофорта сейчас свежий ветер"
+        elif 10.8 <= weather["wind"]['speed'] <= 13.8:
+            wind = "По шкале Бофорта сейчас сильный ветер"
+
+        bot.send_message(message.chat.id,
+                         "В городе " + str(weather["name"]) + " температура: " + str(
+                             int(weather["main"]['temp'])) + '°' + "\n" +
+                         "Максимальная температура:" + str(int(weather['main']['temp_max'])) + "\n" +
+                         "Минимальная температура: " + str(int(weather['main']['temp_min'])) + "\n" +
+                         "Скорость ветра: " + str(int(weather['wind']['speed'])) + ' м/c' + "\n" +
+                         "Давление: " + str(int(weather['main']['pressure'])) + 'мм рт. ст.' + "\n" +
+                         "Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" +
+                         "Видимость: " + str(weather['visibility']) + "\n" +
+                         "Описание: " + str(weather['weather'][0]["description"]) + "\n" + temp + "\n" + wind + '\n')
+    except:
+        # bot.send_message(message.chat.id, 'вы ввели город, которого нет в базе данных \n')
+        # bot.send_message(message.chat.id, 'рестарт... \n')
+        # bot.send_message(message.chat.id, 'введите название города')
+        bot.send_message(message.chat.id,
+                         f'похоже что-то не так с api погоды\n напишите пожалуйста нашему админу{admin}')
+        print(Fore.RED + 'Ошибка отправки погоды')
